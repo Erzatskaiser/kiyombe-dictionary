2983f5d0477de0311d252253b4afe3203d755389
Emmanuel Butsana <ebutsana@gmail.com>
2024-05-24 12:28:26 +0000

    Added search bar

--- frontend/kiyombe-dict/src/pages/search/search.js
+++ frontend/kiyombe-dict/src/pages/search/search.js
@@ -1,11 +1,21 @@
 //Necessary impots
-//import './home.css';
+import './search.css';
 
 //Output function
 function Search() {
   return (
-    <body className="canvas">
- 	<h1> Hello, World! </h1> 
+    <body className="canvas_search">
+      <div className="navbar">
+        <div className="site_title_search">
+ 	        Kiyom.be
+        </div>
+      </div>
+      <div className="search_body">
+        <div className="search_box">
+          <input type="text" className="searched_word"/>
+          <i className="fa fa-search" aria-hidden="true"></i>
+        </div>  
+    </div>
     </body>
   );
 }
