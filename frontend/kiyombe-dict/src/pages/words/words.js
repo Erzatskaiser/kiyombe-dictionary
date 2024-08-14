e2be4449966b4eee639083641ef51c62e72276b0
Emmanuel Butsana <ebutsana@gmail.com>
2024-08-14 11:56:00 -0400

    Completed word pages

--- frontend/kiyombe-dict/src/pages/words/words.js
+++ frontend/kiyombe-dict/src/pages/words/words.js
@@ -1,6 +1,7 @@
 //Necessary imports 
 import {useState, useEffect} from "react";
 import {useNavigate} from "react-router-dom"
+import './words.css'
 
 function Words(props) {
 
@@ -18,18 +19,48 @@ function Words(props) {
     // if there is a word set 
     if(props.word){
       //Query API endpoint to get word data
-      fetch('http://172.20.10.2:5000/find/'+props.word)
+      fetch('http://172.20.2.178:5000/find/'+props.word)
         .then((res) => res.json())
         .then((response) =>
           {
             setData(response);
+            console.log(response)
           }
         )
     }
   }, [props.word]);
-  
+
 
   //JSX render of the page
+  return (
+    <div className="canvas_search">
+      <div className="navbar">
+        <div className="site_title_search">
+          Kiyom.be
+        </div>
+      </div>
+      <div className="word_result">
+        
+        <div className="word">
+          {props.word}
+        </div>
+        
+        {!(data===undefined) && data.synonym!="nan" && <div className="synonym">
+          {data.synonym}
+        </div>}
+
+        {!(data===undefined) && <div className="definition">
+          {data.definition}
+        </div>}
+        
+        {!(data===undefined) && <div className="examples">
+          {data.example1!="nan" && <div className="example1">{data.example1}</div>}
+          {data.example2!="nan" && <div className="example2">{data.example2}</div>}
+          {data.example3!="nan" && <div className="example3">{data.example3}</div>}
+        </div>}
+      </div>
+    </div>
+  );
 
   
 
